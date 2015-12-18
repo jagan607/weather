8f1f86a3bd43fcace6e605be59a4477750c812b2
abhijeet1997 <abhijeetroxx117@gmail.com>
2015-12-18 12:06:00 +0530

    Update SettingActivity.java

--- app/src/main/java/m/sunshine/SettingActivity.java
+++ app/src/main/java/m/sunshine/SettingActivity.java
@@ -29,11 +29,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 
-public class SettingActivity extends AppCompatActivity
+public class SettingActivity extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener {
 
-  private EditText location;
-   private  Button change;
+ 
 
 
     @Override
@@ -41,225 +40,15 @@ public class SettingActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_setting_s);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       //preferences from xml
 
 
-
-         change = (Button) findViewById(R.id.btn_change);
-
-
-
-
-
-
-
-
-
-
-        change.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-         updateweather();
-
-            }
-
-        });
-    }
-
-    private void updateweather(){
-        location = (EditText) findViewById(R.id.pref_location_key);
-        String str =    location.getText().toString();
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
-         str = PreferenceManager.getDefaultSharedPreferences(SettingActivity.this)
-                .getString(getString(R.string.pref_location_key) , getString(R.string.pref_location_default));
-        weatherTask.execute(str);
-
-    }
-
-        public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
-
-            private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-            private String getReadableDateString(long time) {
-                SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-                return shortenedDateFormat.format(time);
-            }
-
-
-            private String formatHighLows(double high, double low) {
-
-                long roundedHigh = Math.round(high);
-                long roundedLow = Math.round(low);
-
-                String highLowStr = roundedHigh + "/" + roundedLow;
-                return highLowStr;
-
-            }
-
-
-            private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
-                    throws JSONException {
-
-                // These are the names of the JSON objects that need to be extracted.
-                final String OWM_LIST = "list";
-                final String OWM_WEATHER = "weather";
-                final String OWM_TEMPERATURE = "temp";
-                final String OWM_MAX = "max";
-                final String OWM_MIN = "min";
-                final String OWM_DESCRIPTION = "main";
-                final String OWM_DATE = "dt";
-
-                JSONObject forecastJson = new JSONObject(forecastJsonStr);
-                JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
-
-
-                String[] resultStrs = new String[numDays];
-                for (int i = 0; i < weatherArray.length(); i++) {
-                    // For now, using the format "Day, description, hi/low"
-                    String day;
-                    String description;
-                    String highAndLow;
-
-                    // Get the JSON object representing the day
-                    JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                    // The date/time is returned as a long.  We need to convert that
-                    // into something human-readable, since most people won't read "1400356800" as
-                    // "this saturday".
-                    long dateTime = dayForecast.getLong(OWM_DATE);
-                    dateTime *= 1000L;
-                    day = getReadableDateString(dateTime);
-                    // description is in a child array called "weather", which is 1 element long.
-                    JSONObject
-                            weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                    description = weatherObject.getString(OWM_DESCRIPTION);
-
-                    // Temperatures are in a child object called "temp".  Try not to name variables
-                    // "temp" when working with temperature.  It confuses everybody.
-                    JSONObject
-                            temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                    double high = temperatureObject.getDouble(OWM_MAX);
-                    double low = temperatureObject.getDouble(OWM_MIN);
-
-                    highAndLow = formatHighLows(high, low);
-                    resultStrs[i] = day + " - " + description + " - " + highAndLow;
-
-                }
-
-                for (String s : resultStrs) {
-                    Log.v(LOG_TAG, "Forecast entry: " + s);
-
-                }
-                return resultStrs;
-            }
-
-            @Override
-            protected String[] doInBackground(String... params) {
-
-
-                if (params.length == 0) {
-                    return null;
-                }
-
-                HttpURLConnection urlConnection = null;
-                BufferedReader reader = null;
-
-// Will contain the raw JSON response as a string.
-                String forecastJsonStr = null;
-                String format = "json";
-                String units = "metric";
-                int numDays = 7;
-
-                try {
-                    // Construct the URL for the OpenWeatherMap query
-                    // Possible parameters are available at OWM's forecast API page, at
-                    // http://openweathermap.org/API#forecast
-
-                    final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?&APPID=cc5a1fd7555998674823df066b7809e7";
-
-                    final String QUERY_PARAM = "q";
-                    final String FORMAT_PARAM = "mode";
-                    final String UNITS_PARAM = "units";
-                    final String DAYS_PARAM = "cnt";
-
-
-                    Uri builturi = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                            .appendQueryParameter(QUERY_PARAM, params[0])
-                            .appendQueryParameter(FORMAT_PARAM, format)
-                            .appendQueryParameter(UNITS_PARAM, units)
-                            .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                            .build();
-
-                    URL url = new URL(builturi.toString());
-                    Log.v(LOG_TAG, "built uri:" + builturi.toString());
-
-                    // Create the request to OpenWeatherMap, and open the connection
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.connect();
-
-                    // Read the input stream into a String
-                    InputStream inputStream = urlConnection.getInputStream();
-                    StringBuffer buffer = new StringBuffer();
-                    if (inputStream == null) {
-                        // Nothing to do.
-                        return null;
-                    }
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                        // But it does make debugging a *lot* easier if you print out the completed
-                        // buffer for debugging.
-                        buffer.append(line + "\n");
-                    }
-
-                    if (buffer.length() == 0) {
-                        // Stream was empty.  No point in parsing.
-                        return null;
-                    }
-                    forecastJsonStr = buffer.toString();
-                    Log.v(LOG_TAG, "Forecast JSON String:" + forecastJsonStr);
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "Error ", e);
-                    // If the code didn't successfully get the weather data, there's no point in attempting
-                    // to parse it.
-                    return null;
-                } finally {
-                    if (urlConnection != null) {
-                        urlConnection.disconnect();
-                    }
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (final IOException e) {
-                            Log.e(LOG_TAG, "Error closing stream", e);
-                        }
-                    }
-
-                }
-                try {
-                    return getWeatherDataFromJson(forecastJsonStr, numDays);
-                } catch (JSONException e) {
-                    Log.e(LOG_TAG, e.getMessage(), e);
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-
-        }
-
-    private void bindPreferenceSummaryToValue(Preference preference) {
+ }
+ 
+ private void bindPreferenceSummaryToValue(Preference preference) {
         // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(this);
-
+ 
         // Trigger the listener immediately with the preference's
         // current value.
         onPreferenceChange(preference,
@@ -267,13 +56,11 @@ public class SettingActivity extends AppCompatActivity
                         .getDefaultSharedPreferences(preference.getContext())
                         .getString(preference.getKey(), ""));
     }
-
-
+ 
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
-
         String stringValue = value.toString();
-
+ 
         if (preference instanceof ListPreference) {
             // For list preferences, look up the correct display value in
             // the preference's 'entries' list (since they have separate labels/values).
@@ -288,5 +75,7 @@ public class SettingActivity extends AppCompatActivity
         }
         return true;
     }
+
+   
 
 }
